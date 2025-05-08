e4d0a70eaf45e742f46fb07e4549e477f51b3148
Elvis Felix <elvisjaspion@gmail.com>
2025-05-08 19:23:29 -0300

    bug  header - OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,11 +58,6 @@ export default function Header() {
             ) : (
               <>
                 <li className="md:mr-12">
-                  <Link to="/additems" className="text-gray-700 dark:text-white hover:underline">
-                    Adcionar Itens
-                  </Link>
-                </li>
-                <li className="md:mr-12">
                   <Link to="/list" className="text-gray-700 dark:text-white hover:underline">
                     Lista
                   </Link>
