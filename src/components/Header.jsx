993e687eafed5f70ffbb69b0e217d837847cc54e
Elvis Felix <elvisjaspion@gmail.com>
2025-05-07 22:49:47 -0300

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'  // certifique-se de que o caminho está correto
+import { useAuth } from '../context/AuthContext'
 
 export default function Header() {
   const { user, logout } = useAuth()
@@ -13,17 +13,32 @@ export default function Header() {
   return (
     <div className="bg-gray-700">
       <header className="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 text-gray-200 md:mx-auto md:flex-row md:items-center">
-        <Link to="/" className="flex cursor-pointer items-center whitespace-nowrap text-2xl font-black text-blue-500">
+        <Link
+          to="/"
+          className="flex cursor-pointer items-center whitespace-nowrap text-2xl font-black text-blue-500"
+        >
           Lembrou?
         </Link>
+
         <input type="checkbox" className="peer hidden" id="navbar-open" />
         <label className="absolute top-5 right-7 cursor-pointer md:hidden" htmlFor="navbar-open">
           <span className="sr-only">Toggle Navigation</span>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </label>
-        <nav className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
+
+        <nav
+          aria-label="Header Navigation"
+          className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start"
+        >
           <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
             {!user ? (
               <li className="md:mr-12">
